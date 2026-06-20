d48e7a2f37955752cc9c68620ddd01281de9bd57
poogpoop <almooooj1@gmail.com>
2026-06-20 11:26:38 +0300

    Refactor create_app to remove SocketIO integration
    Updated the Flask application factory to remove SocketIO and modified the secret key.

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
+"""
+ADVANCED SECURITY TOOLKIT PRO v4.0
+Main Application Package
+"""
+
 from flask import Flask
-from flask_socketio import SocketIO
 import os
 
 def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='static')
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret')
+    """Application Factory - Simple Version for Render"""
     
-    socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+    app = Flask(__name__,
+                template_folder='templates',
+                static_folder='static')
     
+    # Config
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-change-me')
+    
+    # Register Blueprints (Systems)
     from app.systems.vpn_proxy_manager import bp as vpn_bp
     app.register_blueprint(vpn_bp, url_prefix='/vpn_proxy')
     
-    return app, socketio
+    return app
